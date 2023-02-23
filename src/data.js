2ef59ab923fb5353207435a0e1e7938eba145d14
developerHet <hetpatel2312.hp@gmail.com>
2023-02-23 11:58:29 +0530

    Fixed broken image

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "http://surl.li/bwqoz",
+      img: "https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525086765ghqp4.png",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
